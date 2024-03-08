94029c4dd9ea4c248408ed13eecd1673b4f48ef4
KyleFC <35385929+KyleFC@users.noreply.github.com>
2024-03-08 13:10:01 -0800

    Added iamge overlay to map

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup, ImageOverlay } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import customMapOverlay from './osm2.png'; // Import your custom map overlay image
 
 function App() {
+  const position = [33.6540434, -117.8105295];
+  const bounds = [[33.65109, -117.81465], [33.65694, -117.8064]]; // Adjust these bounds to fit your custom map overlay
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MapContainer center={position} zoom={17} style={{ height: '100vh', width: '100%' }}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <Marker position={position}>
+        <Popup>
+          Your university's name here. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+      <ImageOverlay url={customMapOverlay} bounds={bounds} />
+    </MapContainer>
   );
 }
 
