f23d836b8d411340bfafe6e49c8530a1ac04b11a
KyleFC <35385929+KyleFC@users.noreply.github.com>
2024-04-22 13:10:17 -0700

    changed tools locaiton to myapi

--- backend/campus_backend/myapi/views.py
+++ backend/campus_backend/myapi/views.py
@@ -2,7 +2,7 @@ from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .utilities.analysis import get_response
-from campus_backend import Tools
+import Tools
 from django.db import connections
 
 tools_instance = Tools()  # Create an instance of Tools, which manages the initialization of clients.
