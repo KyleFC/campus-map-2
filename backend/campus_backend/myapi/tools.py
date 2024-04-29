4e46fdbfd8d31c5e65eb701dc9354edceacc3d47
KyleFC <35385929+KyleFC@users.noreply.github.com>
2024-04-29 15:43:17 -0700

    1000 max tokens

--- backend/campus_backend/myapi/tools.py
+++ backend/campus_backend/myapi/tools.py
@@ -183,7 +183,8 @@ class Tools:
                            example query: 'All theology classes on Tuesday'
                            expected output: ```SELECT * FROM myapi_session WHERE meetday LIKE '%T%' AND course_id IN (SELECT id FROM Courses WHERE course_code LIKE 'THEO%')```
                            """}, {"role": "user", "content": query}],
-                model="mixtral-8x7b-32768"
+                model="mixtral-8x7b-32768",
+                max_tokens=1000,
             )
             response_message = response.choices[0].message.content
             #print(response_message)
