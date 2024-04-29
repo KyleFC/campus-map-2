e3d6163991d28b2e9b41648ff957637a42917c51
KyleFC <35385929+KyleFC@users.noreply.github.com>
2024-04-29 12:11:09 -0700

    added a little more to query vector prompt

--- backend/campus_backend/myapi/utilities/analysis.py
+++ backend/campus_backend/myapi/utilities/analysis.py
@@ -44,7 +44,7 @@ def get_response(tool=None, message_history=[]):
                     "properties": {
                         "query": {
                             "type": "string",
-                            "description": "The information you hope to get from the database (e.g. Computer Science faculty, President Thomas, application deadline, gala of the stars, campus events)",
+                            "description": "The information you hope to get from the database (e.g. Computer Science faculty, President Thomas, Professor Boleman, application deadline, gala of the stars, campus events, Borland Manske Center)",
                         }
                     },
                     "required": ["query"],
